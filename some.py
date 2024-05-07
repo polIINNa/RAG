336adacd90e9c49f6192b4a668006f86cf2c28cb
Polina <rebrovapolina@gmail.com>
2024-05-07 19:30:09 +0300

    created one sample for reranker

--- some.py
+++ some.py
@@ -1,7 +1,9 @@
 import json
 
 if __name__ == '__main__':
-    print('ПП 26.pdf-0-0'.split('.pdf-')[1])
-
+    with open('chunks_questions/ПП 26.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    test = data[0]
+    print(test['questions'])
 
 
